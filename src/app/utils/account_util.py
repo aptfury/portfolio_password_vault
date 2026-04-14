18a5d72f78bf53f3ab261ee03bbaaafe21bc1bad
Blake <blake.lemarr@outlook.com>
2026-04-14 05:22:27 -0400

    refactored account util for better operation w/ account repo

--- src/app/utils/account_util.py
+++ src/app/utils/account_util.py
@@ -15,6 +15,7 @@ from ..models import (
     AccountStatus
 )
 from ..repositories import AccountRepository
+from typing import Optional
 
 # ========== INIT ========== #
 load_dotenv()
@@ -75,30 +76,27 @@ class AccountUtil:
         # create new account
         return self.repo.create(new_account, req_id='INTERNAL_ADMINISTRATOR')
 
-    def internal_read(self, user_id: str) -> AccountInternal | None:
+    def read_self(self, req: AccountInternal, field: str, value: str) -> Optional[AccountPublic]:
         '''
-        Internally grabs a user by their user_id as part of internal processes.
+        Lets user view their own profile.
 
-        :param user_id:
+        :param req:
+        :param field:
+        :param value:
         :return:
         '''
 
-        # get the account
-        account: list[AccountInternal] = self.repo.read('id', user_id, 'INTERNAL')
+        self._check_access_forbidden(req)
 
-        if account is None:
-            return None
+        account = self.repo.read(field, value, req_id=req.id)
+        return AccountPublic.model_construct(account[0].model_dump(mode='json'))
 
-        # transform account to public view and return it
-        return account[0]
-
-    def read(
+    def admin_search(
             self,
-            user: AccountInternal, # the user querying
+            req: AccountInternal, # the user querying
             field: AcceptedFields, # the field they are querying
             value: str | AccountStatus, # the value they are searching for in that field
-            all_matches: bool = False # returns a list of all responses - ADMIN ONLY
-    ) -> AccountInternal | AccountPublic | list[AccountInternal | AccountPublic] | None:
+    ) -> Optional[list[AccountInternal]]:
         '''
         Queries for a specific user based on input.
 
@@ -107,118 +105,75 @@ class AccountUtil:
         - ON_HOLD and USERS are allowed to query themselves and view their own accounts.
         - BANNED are not allowed to query themselves or anyone else.
 
-        :param user:
+        :param req:
         :param field:
         :param value:
-        :param all_matches:
         :return:
         '''
 
-        is_admin: bool = self._is_admin(user.status)
-        is_forbidden: bool = self._check_access_forbidden(user.status)
+        self._check_access_forbidden(req)
 
-        if is_forbidden:
-            if user.status == AccountStatus.BANNED:
-                # todo - throw error
-                # todo - security log attempt
-                # todo - exit program without further responses
-                return None
+        if not self._is_admin(req):
+            raise PermissionError('You cannot perform this operation unless you are an administrator.')
 
-        if not is_admin:
-            if getattr(user, field).lower() == value.lower() and not all_matches:
-                account: list[AccountInternal] = self.repo.read(field, value, user.id)
+        return self.repo.read(field, value, req_id=req.id)
 
-                # double check that it's their account
-                if user.id != account.id:
-                    # todo - throw error
-                    # todo - security log attempt
-                    # todo - exit program with alert
-                    return None
-
-                # convert to secure view
-                public_access: AccountPublic = AccountPublic.model_construct(account[0].model_dump(mode='json'))
-                return public_access
-
-        # double check request is coming from admin
-        if is_admin:
-            # check if they are requesting a list
-            accounts: list[AccountInternal] = self.repo.read(field, value, user.id)
-
-            if all_matches:
-                return accounts
-            else:
-                return accounts[0]
-        else:
-            # todo - throw error
-            # todo - log error
-            # todo - exit with alert to user that they should report this/open an issue
-            return None
-
-    def update(self, user: AccountInternal, update: AccountInternal) -> bool | None:
+    def update(self, req: AccountInternal, update: AccountInternal) -> bool | None:
         '''
         Updates a user's account with new information without replacing their IDs.
 
         # todo - Make specific models for updating data to simplify process.
 
-        :param user:
-        :param field:
-        :param search:
+        :param req:
         :param update:
         :return:
         '''
 
-        if self._check_access_forbidden(user.status):
-            # todo - manage error
-            return None
+        self._check_access_forbidden(req)
 
-        if not self._is_admin(user.status):
-            if user.id == update.id:
-                if user.status != update.status:
-                    return False
+        # guarantee users cannot change their own status, even as admin
+        if req.id == update.id:
+            update.status = req.status
 
-                response = self.repo.update(update.id, update, user.id)
-                return response
-            else:
-                return None
+        if not self._is_admin(req):
+            if not req.id == update.id:
+                raise PermissionError('You do not have permissions to edit this account.')
 
-        if self._is_admin(user.status):
-            # ensure admin cannot change their own status
-            if user.id == update.id and user.status != update.status:
-                return False
+            return self.repo.update(req.id, update, req_id=req.id)
 
-            response = self.repo.update(update.id, update, user.id)
-            return response
-        else:
-            return None
+        return self.repo.update(update.id, update, req_id=req.id)
 
-    def get_all(self, user: AccountInternal) -> list[AccountInternal] | None:
+    def get_all(self, req: AccountInternal) -> list[AccountInternal] | None:
         '''
         Retrieves all user accounts.
 
-        :param user:
+        :param req:
         :return:
         '''
-        if not self._is_admin(user.status):
-            return None
+
+        self._check_access_forbidden(req)
+
+        if not self._is_admin(req):
+            raise PermissionError('You do not have permissions for this operation.')
 
         return self.repo.get_all()
 
-    def remove(self, user: AccountInternal, reason: str, target: AccountInternal) -> bool | None:
+    def remove(self, req: AccountInternal, reason: str, target: AccountInternal) -> bool | None:
         '''
         Removes a user account.
 
-        :param user:
+        :param req:
         :param reason:
         :param target:
         :return:
         '''
-        if not self._is_admin(user.status):
-            if self._check_access_forbidden(user.status):
-                return False
 
-            if user.id == target.id:
-                return self.repo.delete(target.id, reason, user.id)
+        self._check_access_forbidden(req)
 
-            return None
+        if not self._is_admin(req):
+            if not req.id == target.id:
+                raise PermissionError('You do not have permissions for this operation.')
 
-        return self.repo.delete(target.id, reason, user.id)
+            return self.repo.delete(req.id, 'User deleting their account.', req_id=req.id)
+
+        return self.repo.delete(target.id, reason, req_id=req.id)
