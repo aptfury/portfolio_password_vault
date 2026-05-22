c09bf25a908ab9bcce3ec07a4ec3e063d2ce7903
Blake <blake.lemarr@outlook.com>
2026-05-22 01:25:40 -0400

    updated main to include docstring

---
+++ src/app/main.py
@@ -0,0 +1,20 @@
+""" main.py
+
+AUTHOR: Blake Lemarr
+CREATED: 05.22.26
+UPDATED: mm.dd.yy
+
+DESCRIPTION: An application that stores user passwords/log in information
+
+UPDATES:
+    - [mm.dd.yy] Update info
+    
+DEPENDENCIES:
+    - Name
+
+"""
+
+# -------------------- IMPORTS -------------------- #
+
+
+# -------------------- TITLE -------------------- #
