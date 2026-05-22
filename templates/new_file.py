4e1fa31f9eec335f09c3a158eb7d8b5b56b3bdbd
Blake <blake.lemarr@outlook.com>
2026-05-22 01:22:52 -0400

    created app package and template for a new file

---
+++ templates/new_file.py
@@ -0,0 +1,20 @@
+""" file_name.py
+
+AUTHOR: Blake Lemarr
+CREATED: 05.22.26
+UPDATED: mm.dd.yy
+
+DESCRIPTION: Information
+
+UPDATES:
+    - [mm.dd.yy] Update info
+    
+DEPENDENCIES:
+    - Name
+
+"""
+
+# -------------------- IMPORTS -------------------- #
+
+
+# -------------------- TITLE -------------------- #
